c7116827c50f66f70d31c5a71ec7117d5485632b
SpringCard <matthieu.b@springcard.com>
2016-04-08 12:15:07 +0200

    Cleaning for new version

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-mkdir src/CCID/
-cd src/CCID/
-git clone https://github.com/LudovicRousseau/CCID.git
-cd ..
-git clone https://github.com/LudovicRousseau/PCSC.git
-cd ..
-make
-
--- src/config.h
+++
@@ -1,6 +0,0 @@
-#ifndef __LOCAL_CONFIG_H__
-#define __LOCAL_CONFIG_H__
-#include <stddef.h>
-typedef unsigned char    uint8_t;
-
-#endif
